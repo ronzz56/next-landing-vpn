ce98ca0c90ff497ff98fa0e883c76a1efb5ef6b7
ronzz56 <mahekalonga71@gmail.com>
2024-09-29 15:03:27 +0700

    Update SeoHead.js

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -4,18 +4,18 @@ import { useRouter } from "next/router";
 
 // Default value for some meta data
 const defaultMeta = {
-  title: 'LaslesVPN',
-  siteName: 'LaslesVPN',
+  title: 'RonzzAPI',
+  siteName: 'RonzzAPI',
   description:
-    'Landing page VPN LaslesVPN Best VPN For Privacy, Country and Cheapest',
+    'Landing page for RonzzAPI',
   // change base url of your web (without '/' at the end)
-  url: 'https://next-landing-vpn.vercel.app',
+  url: 'https://next-landing-vpn-beta.vercel.app',
   type: 'website',
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: 'https://next-landing-vpn.vercel.app/assets/card-image.png',
-  author: 'Lorem Ipsum'
+  image: 'https://next-landing-vpn-beta.vercel.app/assets/card-image.png',
+  author: 'Ronzz YT'
 };
 
 /**
