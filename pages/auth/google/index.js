df4570326d128a774106af491adc897aee00749e
ronzz56 <mahekalonga71@gmail.com>
2024-10-02 20:38:39 +0700

    Update index.js

--- pages/auth/google/index.js
+++ pages/auth/google/index.js
@@ -1,5 +1,4 @@
 import passport from "passport";
 import { db } from "../lib/database";
-import config from "../config";
 
 export async function GET(passport.authenticate("google", { scope: ["profile", "email"] }))
