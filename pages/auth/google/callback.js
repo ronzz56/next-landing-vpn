ecf4cf36127899d384f72fb43f55321f039b78e8
ronzz56 <mahekalonga71@gmail.com>
2024-10-02 21:14:22 +0700

    Delete pages/auth/google directory

--- pages/auth/google/callback.js
+++
@@ -1,8 +0,0 @@
-import passport from "passport";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  if (passport.authenticate("google", { failureRedirect: "/" })) {
-    return NextResponse.redirect("/signin")
-  }
-}
--- pages/auth/google/index.js
+++
@@ -1,6 +0,0 @@
-import passport from "passport";
-import { db } from "../lib/database";
-
-export async function GET() {
-  return passport.authenticate("google", { scope: ["profile", "email"] })
-}
