26c53f31a3912a0481593bbb990f67273d4bc609
ronzz56 <mahekalonga71@gmail.com>
2024-10-02 20:51:24 +0700

    Update callback.js

--- pages/auth/google/callback.js
+++ pages/auth/google/callback.js
@@ -1,6 +1,8 @@
 import passport from "passport";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(passport.authenticate("google", { failureRedirect: "/" }), req: NextRequest) {
-  NextResponse.redirect("/signin")
+export async function GET(req: NextRequest) {
+  if (passport.authenticate("google", { failureRedirect: "/" })) {
+    return NextResponse.redirect("/signin")
+  }
 }
